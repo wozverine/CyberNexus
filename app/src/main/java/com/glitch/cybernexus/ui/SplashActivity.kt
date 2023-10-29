c862d76d843e7629e447cef677b18271397730d7
wozverine <wozverine@users.noreply.github.com>
2023-10-29 23:58:22 +0300

    UI

--- app/src/main/java/com/glitch/cybernexus/ui/SplashActivity.kt
+++ app/src/main/java/com/glitch/cybernexus/ui/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.glitch.cybernexus.ui
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
@@ -7,10 +8,11 @@ import androidx.appcompat.app.AppCompatActivity
 import com.glitch.cybernexus.databinding.ActivitySplashBinding
 
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashBinding
-    private val ANIMATION_TIME: Long = 3000 //Change time according to your animation.
+    private val ANIMATION_TIME: Long = 3000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
--- app/src/main/java/com/glitch/cybernexus/ui/signup/SignUpFragment.kt
+++ app/src/main/java/com/glitch/cybernexus/ui/signup/SignUpFragment.kt
@@ -5,14 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.glitch.cybernexus.R
 import com.glitch.cybernexus.databinding.FragmentSignupBinding
 
 class SignUpFragment : Fragment() {
     private var _binding: FragmentSignupBinding? = null
     private val binding get() = _binding!!
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         _binding = FragmentSignupBinding.inflate(inflater, container, false)
         return binding.root
@@ -20,7 +21,11 @@ class SignUpFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        with(binding) {
+            loginNextBtn.setOnClickListener() {
+                findNavController().navigate(R.id.action_signUpFragment_to_homeFragment)
+            }
+        }
     }
 
     override fun onDestroyView() {
