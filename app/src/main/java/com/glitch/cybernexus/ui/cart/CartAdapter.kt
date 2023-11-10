5bac6549dcc48d1f967b9b78b0101bdcb942b887
wozverine <wozverine@users.noreply.github.com>
2023-11-10 14:50:57 +0300

    UI

---
+++ app/src/main/java/com/glitch/cybernexus/ui/cart/CartAdapter.kt
@@ -0,0 +1,49 @@
+package com.glitch.cybernexus.ui.cart
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.glitch.cybernexus.data.model.Product
+import com.glitch.cybernexus.databinding.ItemProductHomeBinding
+
+class CartAdapter(
+    private val onCartClicked: (String) -> Unit
+) : RecyclerView.Adapter<CartAdapter.CartViewHolder>() {
+    private val cartList = mutableListOf<Product>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CartAdapter.CartViewHolder {
+        val binding =
+            ItemProductHomeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CartViewHolder(binding, onCartClicked)
+    }
+
+    override fun onBindViewHolder(holder: CartAdapter.CartViewHolder, position: Int) {
+        holder.bind(cartList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return cartList.size
+    }
+
+    class CartViewHolder(
+        private val binding: ItemProductHomeBinding, val onCartClicked: (String) -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(product: Product) {
+            with(binding) {
+                tvProductName.text = product.title
+                tvCompany.text = product.company
+
+                root.setOnClickListener {
+                    onCartClicked(product.company)
+                }
+            }
+
+        }
+    }
+
+    fun updateList(list: List<Product>) {
+        cartList.clear()
+        cartList.addAll(list)
+        notifyItemRangeChanged(0, list.size)
+    }
+}
--- app/src/main/java/com/glitch/cybernexus/ui/cart/CartFragment.kt
+++ app/src/main/java/com/glitch/cybernexus/ui/cart/CartFragment.kt
@@ -4,12 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.glitch.cybernexus.data.source.Database
 import com.glitch.cybernexus.databinding.FragmentCartBinding
 
 class CartFragment : Fragment() {
     private var _binding: FragmentCartBinding? = null
     private val binding get() = _binding!!
+
+    private val cartAdapter = CartAdapter(
+        onCartClicked = ::onCartClicked
+    )
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,10 +27,24 @@ class CartFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            cartRv.adapter = cartAdapter
+            Database.addProduct("product", "description", "company")
+            Database.addProduct("product", "description", "company")
+            Database.addProduct("product", "description", "company")
+            Database.addProduct("product", "description", "company")
+            Database.addProduct("product", "description", "company")
+            cartAdapter.updateList(Database.getProduct())
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun onCartClicked(desc: String) {
+        Toast.makeText(requireContext(), desc, Toast.LENGTH_SHORT).show()
     }
 }
